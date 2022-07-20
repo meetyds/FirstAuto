a36980a6cd4879a243e952b8b2cd2bafad924fa7
Meet Patel <meet.yds@gmail.com>
2022-07-20 19:04:44 +0100

    Second Line Commited

--- Print.java
+++ Print.java
@@ -3,5 +3,6 @@ package Concept;
 public class Print {
     public static void main(String[] args) {
         System.out.println("My First Automation Learning");
+        System.out.println("My Second Automation Learning");
     }
 }
